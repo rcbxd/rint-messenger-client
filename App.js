90b6de7c91def48cfb2d45b3111a865ba5dae51a
rcbxd <rbcxdd@gmail.com>
2021-05-04 22:36:54 -0500

    android and ios safe area

--- App.js
+++ App.js
@@ -2,19 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { Header } from "./components/Header.js";
 import { Body } from "./components/Body.js"
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Platform } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Header></Header>
-      <Body></Body>
-    </View>
-  );
-}
+import { colors } from "./utils/colors";
 
 const styles = StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: colors.bg,
+    paddingTop: Platform.OS === 'android' ? 25 : 0
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -22,3 +19,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.droidSafeArea}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Header></Header>
+        <Body></Body>
+      </View>
+    </SafeAreaView>
+  );
+}
